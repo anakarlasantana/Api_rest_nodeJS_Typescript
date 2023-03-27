9bfa88603082d9185511cd84f6c49457a3109b5d
Ana Karla <ana.karla@portfoliotech.com.br>
2023-03-27 19:50:08 -0300

    feat: yup validation adition

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,12 +1,31 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from 'yup';
 
 interface ICidades {
   nome: string;
 }
 
-export const create = (req: Request<{}, {}, ICidades>, res: Response) => {
+const bodyValidation: yup.Schema<ICidades> = yup.object().shape({
+  nome: yup.string().required().min(3),
+});
 
-  console.log(req.body);
+export const create = async (req: Request<{}, {}, ICidades>, res: Response) => {
+  let validatedData: ICidades | undefined = undefined;
+
+  try {
+    validatedData = await bodyValidation.validate(req.body);
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+
+    return res.json({
+      errors: {
+        default: yupError.message,
+      }
+    })
+  }
+
+  console.log(validatedData);
   
 
   return res.send('Created!');
