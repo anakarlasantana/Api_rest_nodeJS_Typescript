a0f0afafe9869f5ca75dec7196bb6aa0e08c71f0
Ana Karla <ana.karla@portfoliotech.com.br>
2023-04-24 17:45:22 -0300

    feat: add delete and update controll

--- src/server/controllers/cidades/index.ts
+++ src/server/controllers/cidades/index.ts
@@ -1,10 +1,14 @@
 import * as create from './Create';
+import * as deleteById from './DeleteById';
 import * as getAll  from './GetAll';
 import * as getById  from './GetById';
+import * as updateById from './UpdateById';
 
 export const CidadesController = {
- ...create, 
- ...getAll,
- ...getById,
+  ...deleteById,
+  ...updateById,
+  ...getById,
+  ...create,
+  ...getAll,
 };
 
