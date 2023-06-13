56e03ecc3ee8d7e6cfde4dfcc188ed7ac5c7b307
Ana Karla <ana.karla@portfoliotech.com.br>
2023-06-13 13:32:57 -0300

    feat: migrations com Knex, Node e Typescript

---
+++ src/server/database/knex/@types/knex.d.ts
@@ -0,0 +1,8 @@
+
+declare module 'knex/types/tables' {
+  interface Tables { //Tipagens para as tabelas
+    // cidade: ICidades
+     // pessoa: IPessoa
+      // usuario: IUsuario
+  }
+}
---
+++ src/server/database/knex/Environment.ts
@@ -0,0 +1,32 @@
+import {Knex} from 'knex';
+import path from 'path';
+
+export const development: Knex.Config = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: path.resolve(__dirname, '..', '..', '..', '..', 'database.sqlite')
+  },
+  migrations: {
+    directory: path.resolve(__dirname,'..', 'migrations'),
+  },
+  seeds: {
+    directory: path.resolve(__dirname, '..', 'seeds'),
+  },
+  // No caso de SQLite é necessário fazer uma configuração para atualização de foreign key//
+  pool: {
+    afterCreate: (connection: any, done: Function) => {
+      connection.run('PRAGMA foreign-key = ON');
+      done();
+    }
+  }
+};
+
+export const test: Knex.Config = {
+  ...development,
+  connection: ':memory:',
+};
+
+export const production: Knex.Config = {
+  ...development,
+};
